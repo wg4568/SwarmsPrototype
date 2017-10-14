0c2637a97d9502805a36e6bc1aca4f0576bb5c3c
William Gardner <wg4568@gmail.com>
2017-10-14 12:15:31 -0700

    replaced swarmer class with single rigidbody

--- client/swarms.js
+++ client/swarms.js
@@ -15,88 +15,80 @@ function RandomPosition(xmin, xmax, ymin, ymax) {
 	return new Elemental.Vector(xpos, ypos);
 }
 
-var background = RandomPastelColor();
-
-class Swarmer {
-	constructor() {
-		this.rigid = new Elemental.Rigidbody(Elemental.Vector.Empty);
-		this.rigid.friction = 0.95;
-
-		this.sprite = new Elemental.Sprite.Points([
-			new Elemental.Vector(0, 0),
-			new Elemental.Vector(10, 0),
-			new Elemental.Vector(5, 20)
-		], {
-			center: new Elemental.Vector(5, 0),
-			lineWidth: 0,
-			fillColor: RandomPastelColor()
-		});
-		this.sprite.fillColor.value = 180;
-	}
-}
+var background_color = RandomPastelColor();
 
 class Swarm {
-	constructor(size) {
+	constructor(size, controller) {
 		this.swarmers = [];
+		this.controller = controller;
 
 		for (var i = 0; i < size; i++) {
-			var swarmer = new Swarmer();
-			console.log(swarmer.rigid.velocity);
+			var swarmer = new Elemental.Rigidbody(RandomPosition(-10, 10, -10, 10));
+			swarmer.sprite = new Elemental.Sprite.Points([
+				new Elemental.Vector(0, 0),
+				new Elemental.Vector(10, 0),
+				new Elemental.Vector(5, 20)
+			], {
+				center: new Elemental.Vector(5, 0),
+				lineWidth: 0,
+				fillColor: RandomPastelColor()
+			});
+			swarmer.sprite.fillColor.value = 180;
+			swarmer.friction = 0.95;
 			this.swarmers.push(swarmer);
 		}
 	}
 
 	frame(game) {
-		// console.log(this.swarmers[0].rigid.posn);
 		var parent = this;
 		this.swarmers.forEach(function(swarmer) {
-			var mousePos = game.viewport.canvasToWorld(game.mousePos)
-			var angle = Elemental.Helpers.AngleBetween(mousePos, swarmer.rigid.posn) - 90;
-			swarmer.sprite.rotation = angle;
-
-			var toMouse = Elemental.Helpers.StepBetween(mousePos, swarmer.rigid.posn);
-
-			if (game.mouseHeld(Elemental.Mousecodes.RIGHT)) toMouse = Elemental.Vector.Multiply(toMouse, -1);
-			if (game.mousePressed(Elemental.Mousecodes.LEFT)) toMouse = Elemental.Vector.Multiply(toMouse, 10);
-
-			toMouse = Elemental.Vector.Multiply(toMouse, 1);
-
-			swarmer.rigid.addForce(toMouse);
-
-			var repulses = [];
-			parent.swarmers.forEach(function(compare) {
-				debugger;
-				console.log(compare, repulses);
-				var dist = Elemental.Helpers.DistanceBetween(swarmer.rigid.posn, compare.rigid.posn);
-				if (dist < 2 && parent != swarmer) {
-					console.log(swarmer.rigid.posn, compare.rigid.posn);
-					var repulse = Elemental.Helpers.StepBetween(swarmer.rigid.posn, compare.rigid.posn);
-					repulse = Elemental.Vector.Multiply(repulse, -5);
-					repulses.push(repulse);
-				}
-			});
-
-			repulses.forEach(function(repulse) {
-				console.log(repulse);
-				debugger;
-				swarmer.rigid.addForce(repulse);
-			});
-
-			swarmer.rigid.logic();
+			parent.controller(parent, swarmer, game);
 		});
 	}
 
 	draw(game) {
 		this.swarmers.forEach(function(swarmer) {
-			game.viewport.drawSprite(swarmer.sprite, swarmer.rigid.posn);
+			swarmer.sprite.rotation = swarmer.rotation;
+			game.viewport.drawSprite(swarmer.sprite, swarmer.posn);
 		});
 	}
 }
 
-var swarm = new Swarm(10);
+function UserController(swarm, swarmer, game) {
+	var mousePos = game.viewport.canvasToWorld(game.mousePos)
+	var angle = Elemental.Helpers.AngleBetween(mousePos, swarmer.posn) - 90;
+	swarmer.rotation = angle;
+
+	var toMouse = Elemental.Helpers.StepBetween(mousePos, swarmer.posn);
+
+	if (game.mouseHeld(Elemental.Mousecodes.RIGHT)) toMouse = Elemental.Vector.Multiply(toMouse, -1);
+	if (game.mousePressed(Elemental.Mousecodes.LEFT)) toMouse = Elemental.Vector.Multiply(toMouse, 10);
+
+	toMouse = Elemental.Vector.Multiply(toMouse, 1);
+
+	swarmer.addForce(toMouse);
+
+	var repulses = [];
+	swarm.swarmers.forEach(function(compare) {
+		var dist = Elemental.Helpers.DistanceBetween(swarmer.posn, compare.posn);
+		if (dist < 2 && compare != swarmer) {
+			var repulse = Elemental.Helpers.StepBetween(swarmer.posn, compare.posn);
+			repulse = Elemental.Vector.Multiply(repulse, -5);
+			repulses.push(repulse);
+		}
+	});
+
+	repulses.forEach(function(repulse) {
+		swarmer.addForce(repulse);
+	});
+
+	swarmer.logic();
+}
+
+var swarm = new Swarm(500, UserController);
 
 game.start(function() {
-	viewport.drawFill(background);
+	viewport.drawFill(background_color);
 
 	swarm.frame(game);
 	swarm.draw(game);
