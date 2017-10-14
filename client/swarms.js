05d4ed81520c5c25d17a74706941c6cec6603eb8
William Gardner <wg4568@gmail.com>
2017-10-13 22:38:22 -0700

    swarms now in one class

--- client/swarms.js
+++ client/swarms.js
@@ -18,7 +18,8 @@ function RandomPosition(xmin, xmax, ymin, ymax) {
 var background = RandomPastelColor();
 
 class Swarmer {
-	constructor(posn) {
+	constructor(posn, swarm) {
+		this.swarm = swarm;
 		this.rigid = new Elemental.Rigidbody();
 		this.rigid.posn = posn;
 		this.rigid.friction = 0.95;
@@ -47,11 +48,13 @@ class Swarmer {
 		if (game.mouseHeld(Elemental.Mousecodes.RIGHT)) toMouse = Elemental.Vector.Multiply(toMouse, -1);
 		if (game.mousePressed(Elemental.Mousecodes.LEFT)) toMouse = Elemental.Vector.Multiply(toMouse, 10);
 
+		toMouse = Elemental.Vector.Multiply(toMouse, 1);
+
 		this.rigid.addForce(toMouse);
 
 		var parent = this;
 		var repulses = [];
-		swarmers.forEach(function(swarmer) {
+		this.swarm.swarmers.forEach(function(swarmer) {
 			var dist = Elemental.Helpers.DistanceBetween(parent.rigid.posn, swarmer.rigid.posn);
 			if (dist < 2 && parent != swarmer) {
 				var repulse = Elemental.Helpers.StepBetween(parent.rigid.posn, swarmer.rigid.posn);
@@ -68,18 +71,33 @@ class Swarmer {
 	}
 }
 
-var swarmers = [];
+class Swarm {
+	constructor(size) {
+		this.swarmers = [];
 
-for(var i = 0; i < 600; i++){
-	swarmers.push(new Swarmer(RandomPosition(-300, 300, -300, 300)));
+		for (var i = 0; i < size; i++) {
+			this.swarmers.push(new Swarmer(RandomPosition(-10, 10, -10, 10), this));
+		}
+	}
+
+	frame(game) {
+		this.swarmers.forEach(function(swarmer) {
+			swarmer.frame(game);
+		});
+	}
+
+	draw(game) {
+		this.swarmers.forEach(function(swarmer) {
+			swarmer.draw(game);
+		});
+	}
 }
 
+var swarm = new Swarm(100);
 
 game.start(function() {
 	viewport.drawFill(background);
 
-	swarmers.forEach(function(swarmer) {
-		swarmer.frame(game);
-		swarmer.draw(game);
-	});
+	swarm.frame(game);
+	swarm.draw(game);
 });
